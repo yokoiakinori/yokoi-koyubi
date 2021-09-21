e8c6f67d4f050dbe6334455389829d9a5ea2f21e
yokoiakinori <yokoiakinori@icloud.com>
2021-09-21 10:45:58 +0900

    バグフィックス

---
+++ src/pages/form.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import Layout from "../components/layout.js";
+import { graphql, Link } from "gatsby";
+
+// markup
+const FormPage = ({ data }) => {
+  return (
+    <main>
+      <Layout>
+        <h1>TOP</h1>
+        <ul>
+          {data.allMicrocmsBlog.edges.map(({ node }) => (
+            <li key={node.blogId}>
+              <Link to={`/blog/${node.blogId}`}>{node.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </Layout>
+    </main>
+  );
+};
+
+export const query = graphql`
+  {
+    allMicrocmsBlog {
+      edges {
+        node {
+          blogId
+          title
+        }
+      }
+    }
+  }
+`;
+
+export default FormPage;
