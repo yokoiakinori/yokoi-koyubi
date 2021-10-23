86cc52017283b6088ca24860fd330d76a78882b2
yokoiakinori <yokoiakinori@icloud.com>
2021-10-23 10:42:27 +0900

    コードのシンタックスハイライト実装

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import "./src/style/common.scss";
+require("prismjs/themes/prism-solarizedlight.css");
--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,23 @@ module.exports = {
         icon: "src/images/icon.png",
       },
     },
-    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-prismjs`,
+            options: {
+              classPrefix: "language-",
+              inlineCodeMarker: null,
+              aliases: {},
+              showLineNumbers: true,
+              noInlineHighlight: false,
+            },
+          },
+        ],
+      },
+    },
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
     {
